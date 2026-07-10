eb8462c0939cc5812a3a146c33c21081508ab870
Dhitera <mradithyanaufal@gmail.com>
2026-07-10 21:33:38 +0700

    feat: initialize API routes with authentication, user, and accommodation endpoints

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
+use App\Http\Controllers\AccomodationController;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\LogoutController;
 use App\Http\Controllers\Auth\RefreshTokenController;
@@ -16,4 +17,14 @@ Route::prefix('auth')->as('auth.')->group(function () {
 
 Route::middleware(['auth:sanctum', 'ability:api:access'])->group(function () {
     Route::get('/user', [UserController::class, 'show'])->name('user.show');
+
+    Route::prefix('admin')->as('admin.')->group(function () {
+        Route::get('/accomodations', [AccomodationController::class, 'adminIndex'])->name('accomodations.index');
+        Route::post('/accomodations', [AccomodationController::class, 'store'])->name('accomodations.store');
+        Route::put('/accomodations/{accomodation:slug}', [AccomodationController::class, 'update'])->name('accomodations.update');
+        Route::delete('/accomodations/{accomodation:slug}', [AccomodationController::class, 'destroy'])->name('accomodations.destroy');
+    });
 });
+
+Route::get('/accomodations', [AccomodationController::class, 'index'])->name('accomodations.index');
+Route::get('/accomodations/{accomodation:slug}', [AccomodationController::class, 'show'])->name('accomodations.show');
