b348cb8c7f09227d9c477166e710c2424130561c
Ahmad Aufa Ghani <ahmadaufaghani1224@gmail.com>
2026-07-27 12:06:01 +0700

    fix(api): fix php stan problem

--- app/Http/Requests/Banner/SaveBannerRequest.php
+++ app/Http/Requests/Banner/SaveBannerRequest.php
@@ -29,7 +29,7 @@ final class SaveBannerRequest extends FormRequest
      * @return array{
      *     title : string,
      *     description: string,
-     *     menu: string
+     *     menu: string,
      *     image: string
      * }
      */
