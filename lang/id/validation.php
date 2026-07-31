b2f57c40c3a5deb78a6fe2d2f49b26e788fbd1d7
Cahyo Susilo <lssyhc.study@gmail.com>
2026-07-31 21:59:13 +0700

    feat(lang): add Indonesian validation messages

--- lang/id/validation.php
+++ lang/id/validation.php
@@ -3,13 +3,22 @@
 declare(strict_types=1);
 
 return [
+    'image' => ':attribute harus berupa gambar.',
+    'integer' => ':attribute harus berupa angka bulat.',
     'max' => [
         'file' => ':attribute tidak boleh lebih dari :max KB.',
         'string' => ':attribute tidak boleh lebih dari :max karakter.',
     ],
+    'min' => [
+        'file' => ':attribute harus berukuran minimal :min KB.',
+        'string' => ':attribute harus berupa teks minimal :min karakter.',
+        'numeric' => ':attribute harus berupa angka minimal :min.',
+        'array' => ':attribute harus memiliki minimal :min item.',
+    ],
     'mimes' => ':attribute harus berupa file dengan tipe: :values.',
     'required' => ':attribute wajib diisi.',
     'string' => ':attribute harus berupa teks.',
+    'uploaded' => ':attribute gagal diunggah.',
     'url' => ':attribute harus berupa URL yang valid.',
     'attributes' => [
         'username' => 'username',
